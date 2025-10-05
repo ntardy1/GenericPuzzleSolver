ccf237bd5a258c493eec7d76e07211e3377d43a0
Nathan Tardy <tardy.nate@gmail.com>
2025-10-05 18:41:04 -0400

    Add command-line options with `boost::program_options`
    - Add `help` option and usage statement

--- source/main.cpp
+++ source/main.cpp
@@ -12,7 +12,28 @@
 #include "sudoSolveConfig.h"
 #include "puzzle.hpp"
 
-int main(){
+int main(int argc, char **argv){
+
+    // ======================== COMMAND LINE ARGUMENTS ========================
+    // Create `options_description` object for defining program options
+    boost::program_options::options_description description ("Options");
+    // Add program options to the `options_description` object
+    description.add_options()
+                ("help,h", "Display this help message.");
+    // Create `variables_map` object for storing program options (name-value)
+    boost::program_options::variables_map vm;
+    // Process command line arguments and store results in `variables_map` object
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, description), vm);
+    // Perform final option processing (e.g. apply default values)
+    boost::program_options::notify(vm);
+
+    // Check for "help" option presence
+    if (vm.count("help")) {
+        std::cout << "Usage: " << argv[0] << " [options]" << std::endl;
+        std::cout << description << std::endl;
+        return 1;
+    }
+    // ====================== END COMMAND LINE ARGUMENTS ======================
 
     std::string line; // string to hold each line of the input file as it is read
     std::array<std::string, BOARD_ROW_NUM> rows;
